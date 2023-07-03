62955dc6efad154b91417479699cc3b039118e9f
sithum-sathsara145 <sithumsathsara145@gmail.com>
2023-07-03 08:05:36 +0530

    created home page structure

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,19 @@
+import Feed from '@components/feed'
 import React from 'react'
 
-export default function Home() {
+export default function 
+() {
   return (
-    <div>Home</div>
+    <section className='w-full flex-center flex-col'>
+      <h1 className=' head_text text-center'>
+        Discover & Share 
+        <br className=' max-md:hidden'/>
+        <span className=' orange_gradient text-center'>
+          Ai prompts
+        </span>
+        </h1>
+        <p className=' desc text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo officiis consequatur consequuntur accusamus optio voluptas perspiciatis tempore, quidem dolore suscipit pariatur modi qui atque, deserunt alias dolorem eveniet assumenda sunt.</p>
+        <Feed />
+    </section>
   )
 }
---
+++ components/PromptsCard.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function PromptsCard() {
+  return (
+    <div>PromptsCard</div>
+  )
+}
